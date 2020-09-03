f1fc3ac2e2e8e35924f07fcc8a98d55fdb0d7855
Aelix25 <asdaf998@yahoo.com>
2020-09-03 15:29:43 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]//la atasarea scriptului ca component ataseaza automat si animator si audiosource
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;//practic format din cele 3 directii(x,y,z)
+    Quaternion m_Rotation = Quaternion.identity;
+
+
+   
+
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+    
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);//x,y,z primeste valori
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);//mathf.approximately compara cele 2 valori float ca sa vada daca sunt aproximativ egale
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+                m_AudioSource.Play();
+        }
+        else
+            m_AudioSource.Stop();
+        
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);//de aici isi da seama in ce directie sa mearga
+        m_Rotation = Quaternion.LookRotation(desiredForward);                                                        
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
